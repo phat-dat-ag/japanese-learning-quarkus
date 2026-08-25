a41bb28ea0ba2e97f860886e0da61d3f3c724744
Nguyen Phat Dat <129189938+phat-dat-ag@users.noreply.github.com>
2026-08-25 09:43:43 +0700

    Revert "Feature/vocabulary import"

--- src/main/java/com/japaneselearning/vocabulary/importer/dto/ImportResult.java
+++
@@ -1,8 +0,0 @@
-package com.japaneselearning.vocabulary.importer.dto;
-
-public record ImportResult(
-        int total,
-        int created,
-        int updated
-) {
-}
--- src/main/resources/db/migration/V4__fix_primary_column_type.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE vocabulary_meanings
-    MODIFY COLUMN is_primary BIT NOT NULL;
-
-ALTER TABLE vocabulary_readings
-    MODIFY COLUMN is_primary BIT NOT NULL;
